2830cd043167e4514289d4e4b453e10e9fecd14b
kkaltenheuser <karenkaltenheuser@gmail.com>
2020-12-05 18:54:06 -0500

    employee test

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,34 @@
 // TODO: Write code to define and export the Employee class
+
+class Employee {
+    // Set up parameters where the user can input data
+    constructor(name, id, email) {
+    // Parent class requires the following:
+        // Name
+        this.name = name;
+        // ID
+        this.id = id;
+        // Email
+        this.email = email;
+    }
+    // getName()
+    getName() {
+        return this.name;
+    }
+    // getId()
+    getId() {
+        return this.id;
+    }
+    // getEmail()
+    getEmail() {
+        return this.email;
+    }
+    // getRole() 
+    getRole() {
+        // returns 'Employee'
+        return 'Employee';
+    }
+}
+
+// Export module
+module.exports = Employee;
