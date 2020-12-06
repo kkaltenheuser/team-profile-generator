7e04458f03bc363f6013797c8d0341872eaff10b
kkaltenheuser <karenkaltenheuser@gmail.com>
2020-12-05 19:21:15 -0500

    inserted tests to app.js

--- Develop/app.js
+++ Develop/app.js
@@ -11,25 +11,111 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+const render = require("./lib/htmlRenderer");
+// array to hold all added employees
+let allEmployees = [];
+// questions to populate employee data
+const start = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the employee's name?",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "What is the employee's ID number?",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "What is the employee's work email address?",
+            name: "email"
+        },
+        {
+            type: "list",
+            message: "What is the employee's role?",
+            choices: ["Manager", "Engineer", "Intern"],
+            name: "employeeType",
+        }
+        // questions specific to the type of employee
+    ]).then(function (data) {
+        if (data.employeeType === "Manager") {
+            inquirer.prompt([
+                {
+                    type: "input",
+                    message: "What is the managers's office number?",
+                    name: "officeNumber"
+                }
+            ]).then(function (managerData) {
+                // new manager w/ user input
+                let newManager = new Manager(data.name, data.id, data.email, managerData.officeNumber);
+                // push new manager to  employee array
+                allEmployees.push(newManager);
+                // ask if the user would like to add another employee
+                nextEmployee();
+            })
+        }
+        if (data.employeeType === "Engineer") {
+            inquirer.prompt([
+                {
+                    type: "input",
+                    message: "What is the engineer's GitHub username?",
+                    name: "github"
+                }
+            ]).then(function (engineerData) {
+                //  new engineer w/ user input
+                let newEngineer = new Engineer(data.name, data.id, data.email, engineerData.github);
+                // push new engineer to employee array
+                allEmployees.push(newEngineer);
+                // ask if user would like to add employee
+                nextEmployee();
+            });
+        }
+        if (data.employeeType === "Intern") {
+            inquirer.prompt([
+                {
+                    type: "input",
+                    message: "Which school is the intern attending?",
+                    name: "school"
+                }
+            ]).then(function (internData) {
+                //  new intern w/ user input
+                let newIntern = new Intern(data.name, data.id, data.email, internData.school);
+                // push new intern to the employee array
+                allEmployees.push(newIntern);
+                // ask if the user would like to add another employee
+                nextEmployee();
+            })
+        }
+    })
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+}
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+// inquire if user like to add or render
+const nextEmployee = () => {
+    inquirer.prompt([
+        {
+            type: "confirm",
+            message: "Would you like to add another employee?",
+            name: "add"
+        }
+    ]).then(function (data) {
+        // if true, run prompt again
+        if (data.add === true) {
+            questions();
+        } else {
+            // if user is done, render HTML
+            fs.writeFile("team.html", render(allEmployees), function (err) {
+                if (err) {
+                    // if error occurs while rendering HTML
+                    return console.log("Please try again.")
+                }
+            });
+            // Send success message
+            console.log("HTML file successfully created.")
+        }
+    })
+}
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+start();
